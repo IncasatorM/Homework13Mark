76cc53ecfb0f9b7895f1a9d796b6da0d555ddfe0
IncasatorM <mkaminskyi97@gmail.com>
2023-06-17 18:22:47 +0300

    Homework14 Not work

--- Src/Homework13.java
+++ Src/Homework13.java
@@ -14,23 +14,25 @@ public class Homework13 {
 
         Random random = new Random();
 
-        for (int i = 0; i < arrayNM.length; i++) {
+        for (int i = 0; i < arrayNM[i].length; i++) {
             for (int j = 0; j < arrayNM.length; j++) {
                 arrayNM[i][j] = random.nextInt(10);
+
+
 
             }
 
         }
 
-        for (int i = 0; i < arrayMN.length; i++) {
+        for (int i = 0; i < arrayMN[i].length; i++) {
             for (int j = 0; j < arrayMN.length; j++) {
-                arrayMN[i][j] = arrayNM[i][j];
+                arrayMN[i][j] = arrayNM[j][i];
             }
-            for (int j = 0; j < arrayMN.length; j++) {
-                System.out.println(Arrays.toString(arrayMN[i]));
+            for (int j = 0; j < arrayNM[i].length; j++) {
+                System.out.println(Arrays.toString(arrayNM[i]));
 
             }
-            for (int j = 0; j < arrayMN.length; j++) {
+            for (int j = 0; j < arrayMN[i].length; j++) {
                 System.out.println(Arrays.toString(arrayMN[i]));
 
             }
